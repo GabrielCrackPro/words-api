91f2a2e911b1d045025b892fcba8163092290575
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2022-04-15 14:55:49 +0200

    Project Started 🎉

---
+++ api/index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+
+const wordsFile = require("./data/words.json");
+const words = Object.keys(wordsFile);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(helmet());
+
+app.get("/api/words", (req, res) => {
+  res.json(words);
+});
+
+app.get("/api/words/:word", (req, res) => {
+  const word = req.params.word;
+  if (words.includes(word)) {
+    res.json(words[word]);
+  } else {
+    res.status(404).json({ error: "Word not found" });
+  }
+});
+
+app.get("/api/letter/:letter", (req, res) => {
+  const letter = req.params.letter;
+  const wordsWithLetter = words.filter((word) => {
+    return word.startsWith(letter);
+  });
+  res.json({ letter, words: wordsWithLetter });
+});
+
+app.get("/api/anagrams/:word", (req, res) => {
+  const word = req.params.word;
+  const anagrams = words.filter((w) => {
+    return w.split("").sort().join("") === word.split("").sort().join("");
+  });
+  res.json({ word, anagrams });
+});
+
+app.get("/api/random", (req, res) => {
+  const randomWordIndex = Math.floor(Math.random() * words.length);
+  const randomLetterIndex = Math.floor(
+    Math.random() * words[randomWordIndex].length
+  );
+  const letter = words[randomWordIndex][randomLetterIndex];
+  const word = words[randomWordIndex];
+  res.json({ letter, word });
+});
+
+app.get("/api/stats", (req, res) => {
+  const total = words.length;
+  const byLetter = {};
+  const posibleWords = {};
+  words.forEach((word) => {
+    word.split("").forEach((letter) => {
+      if (!posibleWords[letter]) {
+        posibleWords[letter] = 1;
+      } else {
+        posibleWords[letter] += 1;
+      }
+    });
+  });
+
+  const shortest = words.reduce((a, b) => {
+    return a.length < b.length ? a : b;
+  });
+
+  const longest = words.reduce((a, b) => {
+    return a.length > b.length ? a : b;
+  });
+
+  for (let i = 0; i < 26; i++) {
+    const letter = String.fromCharCode(i + 97);
+    const wordsWithLetter = words.filter((word) => {
+      return word.startsWith(letter);
+    });
+    byLetter[letter] = wordsWithLetter.length;
+  }
+  res.json({ total, byLetter, posibleWords, shortest, longest });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
